a90b927648519705e7fa731f8538a492396c2b5e
hhiroshell <hhiroshell@gmail.com>
2023-02-24 21:55:57 +0900

    refactor local dev utilities

--- hack/scripts/re-deploy.sh
+++ hack/scripts/re-deploy.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")
 
 source ./init.sh
- 
+
 ps -f | grep "org.apache.catalina.startup.Bootstrap" | grep -v "grep" > /dev/null
 if [ $? -eq 0 ]; then
     ./shutdown.sh
@@ -14,7 +14,7 @@ APP_CONTEXT=reviews
 rm ${CATALINA_BASE}/webapps/${APP_CONTEXT}.war
 rm -rf ${CATALINA_BASE}/webapps/${APP_CONTEXT}
 
-pushd ../
+pushd ../../
     ./mvnw clean package
     cp ./target/${APP_CONTEXT}.war ${CATALINA_BASE}/webapps/
 popd
